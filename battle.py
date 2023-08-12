61b6d9d9ef1cbd5c52f24470c2457869c7d96633
Ferda11 <129895191+Ferda11@users.noreply.github.com>
2023-08-12 12:22:17 -0400

    Add files via upload

---
+++ battle.py
@@ -0,0 +1,9 @@
+chen=input("antre yn fraz :")
+fraz=chen.split()
+fraz_fomate=[]
+for i in fraz :
+    fraz1=i.capitalize()
+    fraz_fomate.append(fraz1)
+chaine = ' '.join(fraz_fomate)
+print("nouvo mo ou a se :"+chaine)
+
